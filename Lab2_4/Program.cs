9903f10fdac574a3e36c8bb1191fa82fc936058f
VolodymyrVyshnenko <vyshnenko2904@gmail.com>
2021-05-26 21:26:26 +0300

    Добавьте файлы проекта.

---
+++ Lab2_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_4
+{
+    public class MyFactory<T> where T : new()
+    {
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+
+    public class MyClass
+    {
+        public int Val { get; set; }
+        public MyClass(int i)
+        {
+            Val = i;
+        }
+        public MyClass() { }
+    }
+
+    public class MyType
+    {
+        public int a;
+
+        public MyType(int a)
+        {
+            this.a = a;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var instance = MyFactory<MyType>.FacrotyMethod();
+
+            Console.WriteLine(instance.GetType());
+            Console.ReadKey();
+        }
+    }
+}
